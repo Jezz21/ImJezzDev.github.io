b20e6a33562778efb190fdfdd2b961500759cd04
Jezz21 <92313558+Jezz21@users.noreply.github.com>
2024-01-24 12:40:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,82 @@ const myNavSmall = document.querySelector(".my-nav-small");
 const navButtonLarge = myNavLarge.getElementsByTagName("a");
 const navButtonSmall = myNavSmall.getElementsByTagName("a");
 const main = document.querySelector("#main");
+const gameList = [];
+
+
+SetGameList();
+
+function SetGameList()
+{
+    const gameContainer = document.querySelector(".games-list-container");
+
+
+    GameDetails();
+
+    
+    for (let index = 0; index < gameList.length; index++) {
+        const gameDiv = document.createElement("div");
+        const gameLink = document.createElement("a");
+        const gameImg = document.createElement("img");
+        const gameParagraph = document.createElement("p");
+
+        gameDiv.setAttribute("class", "game-container");
+        gameDiv.setAttribute("style", "min-height:200px;");
+        gameLink.setAttribute("href", gameList[index].objectLink);
+        gameLink.setAttribute("target", "_blank");
+        gameImg.setAttribute("src", gameList[index].objectImage);
+        gameParagraph.innerHTML = gameList[index].objectName;
+
+        
+        gameDiv.append(gameImg);
+        gameDiv.append(gameParagraph);
+        gameLink.append(gameDiv);
+        gameContainer.append(gameLink);
+    }
+}
+
+function GameDetails(){
+    
+    gameList.push(MakeObject("Monsters Dungeon Battle", 
+    "https://img.gamemonetize.com/cb4lyqk01r8nugfoqxnaps191l2xvq9f/512x384.jpg", 
+    "https://gamemonetize.com/monsters-dungeon-battle-game"
+    ));
+    
+    gameList.push(MakeObject("Cute Animal Rotate Puzzle", 
+    "https://img.gamemonetize.com/jbpo01qvo3gsyh2i47h910usntb5c9v6/512x384.jpg", 
+    "https://gamemonetize.com/cute-animal-rotate-puzzle-game"
+    ));
+    gameList.push(MakeObject("Merge Candies", 
+    "https://img.gamemonetize.com/uxkhiea6pr6cbr81689stbekjdbcfqay/512x384.jpg", 
+    "https://gamemonetize.com/merge-candies-game"
+    ));
+    
+    gameList.push(MakeObject("Ben10 Jigsaw Puzzle", 
+    "https://img.gamemonetize.com/qacvml1er153n36mbuowjp9ex0p20fte/512x384.jpg", 
+    "https://gamemonetize.com/ben10-jigsaw-puzzle-game"
+    ));
+
+    gameList.push(MakeObject("Summer Beach and Girls Puzzle", 
+    "https://img.gamemonetize.com/kkw2btotcpyyzsyzqutslo4rvgtvm4k8/512x384.jpg", 
+    "https://gamemonetize.com/summer-beach-and-girls-puzzle-game"
+    ));
+}
+
+
+function MakeObject(name, imageSource, link)
+{
+    const object =
+    {
+        objectName: name,
+        objectImage: imageSource,
+        objectLink: link
+    };
+
+    return object;
+}
+
 
 AddListener();
-
 
 function AddListener()
 {
@@ -25,7 +98,6 @@ function AddListener()
         });
     }
 }
-
 function SelectedButtonLarge(largeButton, index)
 {
     const blackButtons = myNavLarge.querySelectorAll(".w3-black");
@@ -36,7 +108,6 @@ function SelectedButtonLarge(largeButton, index)
     largeButton.classList.add("w3-black");
     ShowSelectedPage(index);
 }
-
 function SelectedButtonSmall(smallButton, index)
 {
     const smallButtons = myNavSmall.querySelectorAll(".w3-white");
@@ -49,7 +120,6 @@ function SelectedButtonSmall(smallButton, index)
 
     ShowSelectedPage(index);
 }
-
 function ShowSelectedPage(selectedIndex)
 {
     const mainPages = main.querySelectorAll(".main-page");
@@ -62,3 +132,5 @@ function ShowSelectedPage(selectedIndex)
     }
 
 }
+
+
