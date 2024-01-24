802b87de14df86e1ff74da8dcee17303aca61dcd
Jezz21 <92313558+Jezz21@users.noreply.github.com>
2024-01-24 11:08:04 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const myNavLarge = document.querySelector(".my-nav-large");
 const myNavSmall = document.querySelector(".my-nav-small");
 const navButtonLarge = myNavLarge.getElementsByTagName("a");
-const navButtonSmall = myNavLarge.getElementsByTagName("a");
+const navButtonSmall = myNavSmall.getElementsByTagName("a");
 const main = document.querySelector("#main");
 
 AddListener();
@@ -10,21 +10,44 @@ AddListener();
 function AddListener()
 {
     for (let index = 0; index < navButtonLarge.length; index++) {
-        const selected = navButtonLarge[index];
-        selected.addEventListener("click", () => {
-            
-            const blackButtons = myNavLarge.querySelectorAll(".w3-black");
-    
-            blackButtons.forEach(element => {
-                element.classList.remove("w3-black");
-            });
-    
-            selected.classList.add("w3-black");
-    
-            ShowSelectedPage(index);
-    
+        const largeButton = navButtonLarge[index];
+        const smallButton = navButtonSmall[index];
+
+
+        largeButton.addEventListener("click", () => {
+            SelectedButtonLarge(largeButton, index);
+            SelectedButtonSmall(smallButton, index);
+        });
+
+        smallButton.addEventListener("click", () => {
+            SelectedButtonLarge(largeButton, index);
+            SelectedButtonSmall(smallButton, index);
         });
     }
+}
+
+function SelectedButtonLarge(largeButton, index)
+{
+    const blackButtons = myNavLarge.querySelectorAll(".w3-black");
+
+    blackButtons.forEach(element => {
+        element.classList.remove("w3-black");
+    });
+    largeButton.classList.add("w3-black");
+    ShowSelectedPage(index);
+}
+
+function SelectedButtonSmall(smallButton, index)
+{
+    const smallButtons = myNavSmall.querySelectorAll(".w3-white");
+
+    smallButtons.forEach(element => {
+        element.classList.remove("w3-white");
+    });
+    smallButton.classList.add("w3-white");
+
+
+    ShowSelectedPage(index);
 }
 
 function ShowSelectedPage(selectedIndex)
